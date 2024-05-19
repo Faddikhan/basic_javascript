d007c3e32a36692f59a5922e3d85e867e8c63042
Fawad khan <167312941+Faddikhan@users.noreply.github.com>
2024-05-19 11:45:02 +0000

    arrays in depth

---
+++ javascript/arrays2.js
@@ -0,0 +1,35 @@
+const cartoons = ["Chota Bheem", "Oggy", "Shiva"]
+const movies = ["Tiger zinda ha", "Baghi", "Ek tha tiger"]
+
+// cartoons.push(movies)
+// console.log(cartoons);
+
+//  const all = cartoons.concat(movies)
+// console.log(all);
+
+
+
+// ziada ya use hota ha coding ma concate ki jaga dono ak hi km krtay
+// const allNewValues = [...cartoons, ...movies]
+// // console.log(allNewValues);
+
+// const anotherArray = [0, 1, 2, [3, 4, 5], 6, [8, 9, [10, 11], 12]]
+// const newAnotherArray = anotherArray.flat(2)
+// console.log(newAnotherArray);
+
+
+
+// interested case interview
+
+// const newString = "hello world"
+// console.log(Array.from(newString));
+
+// const bool = true
+// console.log(Array.from(bool));
+
+
+let score = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.from(`converted ${score}, ${score2}, ${score3}`));
