58f39ab6e9ef8c13d0ebb5b7718afbb394006bf6
Fawad khan <167312941+Faddikhan@users.noreply.github.com>
2024-05-20 08:34:08 +0000

    api json

---
+++ javascript/api_json.js
@@ -0,0 +1,20 @@
+// api object and Json
+
+const table = {
+    ComputerTable : "Ha ok ha",
+    DiningTable : "Nashta ki ya khanay ki",
+    SitingTable : "Bethnay ki"
+
+}
+
+// table.DiningTable
+const {SitingTable} = table
+console.log(SitingTable);
+
+
+// json 
+{
+    name: "khuch bhi"
+    fullName: "json "
+    
+}
