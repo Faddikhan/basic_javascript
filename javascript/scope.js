67ecf9ae300b70590218ccb27d7e0e0d070b7523
Fawad khan <167312941+Faddikhan@users.noreply.github.com>
2024-05-26 07:42:04 +0000

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,43 @@
+ // global and scope
+
+//  var c = 50
+// let a = 90
+// const b = 100
+
+//  if (true) {
+//     let a = 20
+//     const b = 30
+//     console.log("inner", a, "second", b);
+
+    // var hamay nhi use krna ha kabhi bhi
+    // var c = 40}
+ 
+
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+// function one() {
+//    const username = "jo ay ga form sa"
+
+//    function two() {
+       
+//       const website = "Youtube"
+//       console.log(username);
+//    }
+
+//    // console.log(website);
+//    two()
+// }
+
+// one()
+
+
+if (true) {
+   const username = "User ka name"
+
+   if (true) {
+      const website = "Youtube"
+      console.log(username + website);
+   }
+}
